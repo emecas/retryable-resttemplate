cdd874131a7913b3d34763571f0ff16bde30671b
Emerson Castaneda <emecas@users.noreply.github.com>
2020-11-26 19:30:28 -0500

    Make example works with SpringBoot 1.4.3.RELEASE
    Signed-off-by: Emerson Castaneda <emecas@users.noreply.github.com>

--- src/main/java/com/taetaetae/retryableresttemplate/TestController.java
+++ src/main/java/com/taetaetae/retryableresttemplate/TestController.java
@@ -3,6 +3,8 @@ package com.taetaetae.retryableresttemplate;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
@@ -14,6 +16,8 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class TestController {
+	private final Logger logger = LoggerFactory.getLogger(TestController.class);
+
 
 	@Autowired
 	private RestTemplate retryableRestTemplate;
@@ -21,6 +25,8 @@ public class TestController {
 	@GetMapping(value = "/employees", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public String employees() throws URISyntaxException {
+		logger.info("/employees");
+		logger.info("retryableRestTemplate"+ retryableRestTemplate.getClass());
 		final String baseUrl = "http://dummy.restapiexample.com/api/v1/employeeszzz";
 		URI uri = new URI(baseUrl);
 		ResponseEntity<String> exchange = retryableRestTemplate.exchange(uri, HttpMethod.GET, null, String.class);
